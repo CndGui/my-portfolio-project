865ce91eb4d8ed95b651a18d0638e81373b00c1a
Guilherme Rodrigues <guilhermelindinho2@hotmail.com>
2024-02-15 22:37:18 -0300

    enviando projeto

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,7 +8,7 @@ export default function Projects() {
             <main className="ml-60 max-[765px]:ml-0 max-[765px]:mt-14 py-20 px-36 max-[1220px]:px-20 max-[917px]:px-10 max-[480px]:px-4 flex flex-col gap-10">
                 <div>
                     <strong className="text-3xl">My Projects</strong>
-                    <p className="text-zinc-400">Here are some of the projects I've created. If interested, see more at <a href="https://github.com/CndGui?tab=repositories" target="_blank" className="font-normal text-cyan-500 underline">Github</a></p>
+                    <p className="text-zinc-400">{"Here are some of the projects I ve created. If interested, see more at"} <a href="https://github.com/CndGui?tab=repositories" target="_blank" className="font-normal text-cyan-500 underline">Github</a></p>
                 </div>
 
                 <div className="grid grid-cols-2 max-[1396px]:grid-cols-1 gap-y-6 max-[1396px]:mx-auto">
@@ -17,7 +17,7 @@ export default function Projects() {
 
                         <div className="mt-3">
                             <strong>My Portfolio (Old)</strong>
-                            <p className="mt-3 text-zinc-600 dark:text-zinc-400">My first project, which wasn't done by following a tutorial or copying from somewhere else. Here was when I truly felt that I was learning.</p>
+                            <p className="mt-3 text-zinc-600 dark:text-zinc-400">{"My first project, which wasn't done by following a tutorial or copying from somewhere else. Here was when I truly felt that I was learning."}</p>
                         </div>
 
                         <div className="mt-auto flex gap-4">
