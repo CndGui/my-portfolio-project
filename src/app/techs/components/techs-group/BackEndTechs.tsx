748687ddb967204520f16ba7c840c7290451e965
Guilherme Rodrigues <guilhermelindinho2@hotmail.com>
2024-03-05 22:31:59 -0300

    new techs in back-end sections

--- src/app/techs/components/techs-group/BackEndTechs.tsx
+++ src/app/techs/components/techs-group/BackEndTechs.tsx
@@ -1,7 +1,7 @@
 import { Tech } from "./Tech"
 
 export function BackEndTechs() {
-    const backEndsArray = ["javascript", "typescript", "nodejs"]
+    const backEndsArray = ["javascript", "typescript", "nodejs", "java", "spring", "mysql", "postgresql"]
     const backEndsElement = backEndsArray.map(i => {
         let name = i
         let destiny;
@@ -20,6 +20,28 @@ export function BackEndTechs() {
             case "nodejs": {
                 destiny = "https://nodejs.org/"
                 name = "nodeJS"
+                break
+            }
+
+            case "java": {
+                destiny = "https://docs.oracle.com/en/java/"
+                break
+            }
+
+            case "spring": {
+                destiny = "https://spring.io/"
+                break
+            }
+
+            case "mysql": {
+                destiny = "https://mysql.com/"
+                name = "mySQL"
+                break
+            }
+
+            case "postgresql": {
+                destiny = "https://www.postgresql.org/"
+                name = "postgreSQL"
                 break
             }
         }
--- src/app/techs/components/techs-group/Tech/TechImage.tsx
+++ src/app/techs/components/techs-group/Tech/TechImage.tsx
@@ -5,7 +5,7 @@ interface TechImageProps {
 export function TechImage({ imageName }: TechImageProps) {
     return (
         <div className="p-2 bg-black/50 dark:bg-white/5 rounded-md">
-            <img src={`/images/logos/${imageName}-logo.png`} alt={imageName} className="size-8" />
+            <img src={`/images/logos/${imageName}-logo.png`} alt={imageName} className="size-8 rounded-sm" />
         </div>
     )
 }
