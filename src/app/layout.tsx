6a79d3a23052c9a1dd98c10c8384e327b7b748c2
Guilherme Rodrigues <guilhermelindinho2@hotmail.com>
2024-03-17 23:11:38 -0300

    fixed seo of the portfolio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,14 +7,21 @@ export const metadata: Metadata = {
   description: "Fullstack developer, striving to demonstrate continuous improvement day by day. This is my portfolio, a project created so that I can share with everyone my experiences, projects, and my skills.",
   openGraph: {
     title: "Guilherme - Front-end developer",
-    description: "Fullstack developer, striving to demonstrate continuous improvement day by day. This is my portfolio, a project created so that I can share with everyone my experiences, projects, and my skills."
+    description: "Fullstack developer, striving to demonstrate continuous improvement day by day. This is my portfolio, a project created so that I can share with everyone my experiences, projects, and my skills.",
+    images: [
+      {
+        url: "\images\opengraph-image.png",
+        width: 1200,
+        height: 630
+      }
+    ]
   }
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
-      <body className="font-poppins bg-lightPage dark:bg-darkPage dark:text-white">
+      <body className="font-poppins bg-lightPage dark:text-white">
         <LayoutProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
         </LayoutProvider>
